520fe59491b1325519d9237e534e26bc1fe418b7
mifi6 <46782831+mifi6@users.noreply.github.com>
2020-08-27 19:48:05 +0900

    [5.9] フォームから値を取得

--- tutorial/dev29/sample4/public/javascripts/index.js
+++ tutorial/dev29/sample4/public/javascripts/index.js
@@ -6,15 +6,15 @@ function getValue() {
 
 // フォームに値が設定されている場合にポップアップを表示する
 function popupValue() {
-    const text1Value = getValue();
+    const text1Value = $('#text1').val();
     // ここに条件分岐を記入する
-
+    if (text1Value !== '') {
         alert(text1Value);
-
+    }	
 }
 
 // 要素に値をセットする
 function setValue() {
     const text2Value = 'Rakus!';
-
+    $('#text2').text(text2Value);
 }
