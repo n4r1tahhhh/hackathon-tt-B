b2be0ceb96f70ee42aae5f4cb71186173efae09d
takeyy <t0k6yist@gmail.com>
2020-08-28 14:56:46 +0900

    masterの変更をdevelopにも反映その1

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,10 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('', function (data) {
+    socket.on('entryMyselfEvent', function (data) {
+        console.log('入室クライアントのユーザ名：' + data);
 
+        // 他クライアントが受信する入室表示イベント（receiveEntryEvent）を送信する
+        socket.broadcast.emit('receiveEntryEvent', data);
     });
 };
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,10 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
+    socket.on('exitMyselfEvent', function (data) {
+        console.log('退室クライアントのユーザ名：' + data);
 
+        // 他クライアントが受信する退室表示イベント（receiveExitEvent）を送信する
+        socket.broadcast.emit('receiveExitEvent', data);
     });
 };
