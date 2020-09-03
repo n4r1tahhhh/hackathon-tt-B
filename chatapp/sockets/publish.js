0567e686f4499a9fbd2c104b37c0c78df71e742f
takeyy <t0k6yist@gmail.com>
2020-09-03 20:02:52 +0900

    投稿できない機能の修正v2

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -3,7 +3,7 @@
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('sendMessageEvent', function (data) {
-        if (!data.message_data) {
+        if (!data) {
             return
         }
         // 全クライアントが受信するメッセージ表示イベント（receiveMessageEvent）を送信する
