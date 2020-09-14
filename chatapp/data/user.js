a37fd51a7ed97a9a5692844635a15d04d1f25117
takeyy <t0k6yist@gmail.com>
2020-09-14 23:12:15 +0900

    dbの構造を変更

--- chatapp/data/user.js
+++ chatapp/data/user.js
@@ -6,14 +6,14 @@ const db = new sqlite.Database("./data/Users.sqlite3");
 //SQL文を同期的に実行する
 db.serialize(() => {
   //テーブルがなれけば「user」を作成
-  db.run('CREATE TABLE IF NOT EXISTS user(id INTEGER UNIQUE, username TEXT)')
+  db.run('CREATE TABLE IF NOT EXISTS user(id INTEGER, username TEXT NOT NULL, time DATETIME, PRIMARY KEY (id))')
 
   //Prepareオブジェクト
-  const data = db.prepare('INSERT INTO user VALUES (?, ?)');
+  const data = db.prepare('INSERT INTO user VALUES (?, ?, ?)');
   try {
-    data.run([1, "hogehoge"]);
-    data.run([2, "fuga"]);
-    data.run([3, "John Doe"]);
+    data.run([1, "hogehoge", "2020-09-01 00:00:00"]);
+    data.run([2, "fuga", "2020-09-02 00:00:00"]);
+    data.run([3, "John Doe", "2020-09-03 00:00:00"]);
   } catch (e) {
     logErrors(e);
   }
@@ -21,7 +21,7 @@ db.serialize(() => {
 
   // 参照
   db.each("SELECT * FROM user", function (err, row) {
-    console.log(row.id + ":" + row.username);
+    console.log(row.id + ":" + row.username + ":" + row.time);
   });
 
   //Prepareオブジェクトを閉じる
